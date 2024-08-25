dc60cb9acbf96d0328ec04500e2b183ad5d6bd79
Snowly <visionhyronic@gmail.com>
2024-08-25 23:21:22 +0200

    added authors and description

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,8 +2,8 @@ fx_version "cerulean"
 game "gta5"
 lua54 'yes'
 
-author 'Randolio\'s Dad'
-description ''
+author 'Snowly, Redacted, Yeet and obviously Randolio Dad'
+description 'Script for Randolios dad to come back'
 
 shared_scripts {
     '@ox_lib/init.lua',
